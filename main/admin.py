96e9f0a4cf1e54549358616d3776be8ce951cc39
Rustamovs1 <rustamovaliyor3@gmail.com>
2024-02-28 11:15:33 +0500

    a

---
+++ main/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Information)
+admin.site.register(Banner)
+admin.site.register(PaymentType)
+admin.site.register(Advertising)
+admin.site.register(Contact)
+admin.site.register(Company)
+admin.site.register(ComplexType)
+admin.site.register(Category)
+admin.site.register(Stata)
+admin.site.register(ComplexImage)
+admin.site.register(FeatureImage)
+admin.site.register(EnvironmentImg)
+admin.site.register(Complex)
+admin.site.register(Services)
+admin.site.register(AboutMe)
+admin.site.register(RealtorAbout)
+admin.site.register(Realtor)
